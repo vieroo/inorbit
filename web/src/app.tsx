24a86112e6713a468b8a0352889064bdfa3ad55f
Eduardo Viero <duduviero@gmail.com>
2024-09-15 22:22:08 -0300

    Finalizado

---
+++ web/src/app.tsx
@@ -0,0 +1,23 @@
+import { Dialog } from "./components/ui/dialog";
+import { CreateGoal } from "./components/create-goal";
+// import { EmptyGoals } from "./components/empty-goals";
+import { Summary } from "./components/summary";
+import { EmptyGoals } from "./components/empty-goals";
+import { useQuery } from "@tanstack/react-query";
+import { getSummary } from "./http/get-summary";
+
+export function App() {
+  const { data } = useQuery({
+    queryKey: ["summary"],
+    queryFn: getSummary,
+    staleTime: 1000 * 60,
+  });
+
+  return (
+    <Dialog>
+      {data?.total && data.total > 0 ? <Summary /> : <EmptyGoals />}
+
+      <CreateGoal />
+    </Dialog>
+  );
+}
---
+++ web/src/components/in-orbit-icon.tsx
@@ -0,0 +1,96 @@
+export function InOrbitIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="25"
+      fill="none"
+      viewBox="0 0 24 25"
+    >
+      <title>in.orbit</title>
+      <path
+        fill="url(#paint0_linear_84_262)"
+        d="M12 .5c-.936 0-1.847.107-2.72.31A12.016 12.016 0 00.31 9.78C.107 10.652 0 11.563 0 12.5c0 6.627 5.373 12 12 12 .936 0 1.847-.107 2.72-.31a12.017 12.017 0 008.97-8.968c.203-.875.31-1.786.31-2.722 0-6.627-5.373-12-12-12zm0 19.515a7.494 7.494 0 01-2.524-.435 7.535 7.535 0 01-4.556-4.555A7.515 7.515 0 1119.515 12.5 7.515 7.515 0 0112 20.015z"
+      ></path>
+      <path
+        fill="#FEFCFE"
+        d="M12 7.657a4.844 4.844 0 100 9.687 4.844 4.844 0 000-9.687zm0 7.772a2.929 2.929 0 110-5.858 2.929 2.929 0 010 5.858z"
+      ></path>
+      <path
+        fill="url(#paint1_linear_84_262)"
+        d="M14.524 5.42a7.515 7.515 0 00-9.604 9.605 7.423 7.423 0 01-2.63-1.702A7.408 7.408 0 01.31 9.78 12.016 12.016 0 019.28.81a7.409 7.409 0 013.543 1.98 7.423 7.423 0 011.701 2.63z"
+      ></path>
+      <path
+        fill="url(#paint2_linear_84_262)"
+        d="M23.69 15.221a12.016 12.016 0 01-8.97 8.969 7.408 7.408 0 01-3.543-1.98 7.427 7.427 0 01-1.701-2.63 7.515 7.515 0 009.605-9.605c.958.362 1.857.93 2.629 1.702a7.41 7.41 0 011.98 3.544z"
+      ></path>
+      <path
+        fill="url(#paint3_linear_84_262)"
+        d="M6.064 20.948a2.511 2.511 0 100-5.023 2.511 2.511 0 000 5.023z"
+      ></path>
+      <path
+        fill="url(#paint4_linear_84_262)"
+        d="M17.958 9.054a2.511 2.511 0 100-5.023 2.511 2.511 0 000 5.023z"
+      ></path>
+      <defs>
+        <linearGradient
+          id="paint0_linear_84_262"
+          x1="20.37"
+          x2="3.89"
+          y1="4.591"
+          y2="20.163"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#F38856"></stop>
+          <stop offset="0.23" stopColor="#F0625C"></stop>
+          <stop offset="1" stopColor="#5C3DAB"></stop>
+        </linearGradient>
+        <linearGradient
+          id="paint1_linear_84_262"
+          x1="11.241"
+          x2="1.867"
+          y1="2.534"
+          y2="11.858"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#F67986"></stop>
+          <stop offset="1" stopColor="#8951BA"></stop>
+        </linearGradient>
+        <linearGradient
+          id="paint2_linear_84_262"
+          x1="21.962"
+          x2="12.588"
+          y1="13.313"
+          y2="22.637"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#F67986"></stop>
+          <stop offset="1" stopColor="#8951BA"></stop>
+        </linearGradient>
+        <linearGradient
+          id="paint3_linear_84_262"
+          x1="7.898"
+          x2="4.23"
+          y1="16.72"
+          y2="20.152"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#A267B9"></stop>
+          <stop offset="1" stopColor="#704BB4"></stop>
+        </linearGradient>
+        <linearGradient
+          id="paint4_linear_84_262"
+          x1="19.485"
+          x2="16.981"
+          y1="5.113"
+          y2="7.456"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#F7A8A5"></stop>
+          <stop offset="0.97" stopColor="#D074C4"></stop>
+          <stop offset="1" stopColor="#CF76A9"></stop>
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+}
